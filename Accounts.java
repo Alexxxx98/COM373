5e7915fa6cebf9fd714f47eb2e0b07e5f6b3e5e5
Alex <alexscott98715@gmail.com>
2018-11-07 17:13:45 +0000

    New change

--- Accounts.java
+++ Accounts.java
@@ -5,18 +5,7 @@ public class Accounts
     public static void main(String[] args) 
     {
         Accounts a1 = new Accounts();
-        //change 1
-        //change 1
-        //change 1
-        //change 1
-        //change 1
-        //change 1
-        //change 1
-        //change 1
-        //change 1
-        //change 1
-        //change 1
-        //change 1
+
         
     } 
     
